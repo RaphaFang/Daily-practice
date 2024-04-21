355c642b23f933342f114e90cfac4873674be231
RaphaFang <fangsihyu0211@gmail.com>
2024-04-21 15:33:04 +0800

    3

--- pdf_download.py
+++ pdf_download.py
@@ -22,47 +22,67 @@
 
 
 # ------------------------------------------------------------------------------------------------------------------------------
+# from selenium import webdriver
+# from selenium.webdriver.common.by import By
+# from selenium.webdriver.support.ui import WebDriverWait
+# from selenium.webdriver.support import expected_conditions as EC
+# import time
+
+# # from zenrows import ZenRowsClient
+# # client = ZenRowsClient("89046282b1402b11fe09b238bcb8a580acebe6bf")
+# # url = "https://www.zeczec.com/users/sign_in"
+# # params = {"premium_proxy":"true","proxy_country":"tw"}
+# # response = client.get(url, params=params)
+
+# url = "https://www.zeczec.com/users/sign_in"
+# driver = webdriver.Chrome()
+# driver.get(url)
+
+# email_blank = driver.find_element("id", "user_email")
+# email_blank.send_keys("nickmomo1524@gmail.com")
+# # time.sleep( 10 )
+# password_blank = driver.find_element("id", "user_password")
+# password_blank.send_keys("1aqzwx2s")
+# # time.sleep( 8 )
+# button = driver.find_element("name","commit")
+# button.click()
+# # time.sleep(10)
+
+# # check_button = driver.find_element(By.XPATH,"//input[@type='checkbox']")  #find_element("type","checkbox")
+# # check_button.click()
+# check_button = WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.XPATH, "//input[@type='checkbox']")))
+# check_button.click()
+
+# time.sleep( 15 )
+# time.sleep( 15 )
+
+
+# ------------------------------------------------------------------------------------------------------------------------------
+
 from selenium import webdriver
 from selenium.webdriver.common.by import By
-# from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 import time
-# from selenium.webdriver.chrome.options import Options
-from zenrows import ZenRowsClient
-
-client = ZenRowsClient("89046282b1402b11fe09b238bcb8a580acebe6bf")
-url = "https://www.zeczec.com/users/sign_in"
-params = {"premium_proxy":"true","proxy_country":"tw"}
-
-response = client.get(url, params=params)
 
 url = "https://www.zeczec.com/users/sign_in"
 driver = webdriver.Chrome()
 driver.get(url)
 
-email_blank = driver.find_element("id", "user_email")
-email_blank.send_keys("nickmomo1524@gmail.com")
-time.sleep( 10 )
-password_blank = driver.find_element("id", "user_password")
-password_blank.send_keys("1aqzwx2s")
-time.sleep( 8 )
-button = driver.find_element("name","commit")
+# Wait for user to manually input email and password
+input("Please enter your email and password manually. Press Enter when done.")
+
+# Once manual input is done, proceed to click the login button
+button = driver.find_element("name", "commit")
 button.click()
-# driver.implicitly_wait(5)
-time.sleep(10 )
-check_button = driver.find_element(By.XPATH,"//input[@type='checkbox']")  #find_element("type","checkbox")
+
+# Wait for the checkbox to appear and click it
+check_button = WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.XPATH, "//input[@type='checkbox']")))
 check_button.click()
-time.sleep( 15 )
-time.sleep( 15 )
 
+# Add any necessary delays for subsequent actions or to observe the behavior
+time.sleep(15)
 
-# driver.implicitly_wait(10) # seconds
-
-# driver.save_screenshot("test.png")
-
-
-
-
-print(response.text)
 
 
 # ------------------------------------------------------------------------------------------------------------------------------
