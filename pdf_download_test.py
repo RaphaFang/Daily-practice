e2fd496a4eb70043b4513f83a12644483c6dbba3
RaphaFang <fangsihyu0211@gmail.com>
2024-04-21 19:01:05 +0800

    6

--- pdf_download_test.py
+++ pdf_download_test.py
@@ -1,10 +1,14 @@
+# ------------------------------------------------------------------------------------------------------------------------------
+# 可以運作，但是讀到的是未登入的頁面
+# ------------------------------------------------------------------------------------------------------------------------------
 # pip install zenrows
-# from zenrows import ZenRowsClient
+from zenrows import ZenRowsClient
+import html
 
-# client = ZenRowsClient("89046282b1402b11fe09b238bcb8a580acebe6bf")
-# url = "https://www.zeczec.com/users/sign_in"
-# params = {"premium_proxy":"true","proxy_country":"tw","antibot":"true", "premium_proxy":"true",  "js_render":"true","autoparse":"true"}
+client = ZenRowsClient("89046282b1402b11fe09b238bcb8a580acebe6bf")
+url = "https://www.zeczec.com/users/sign_in"
+params = {"premium_proxy":"true","proxy_country":"tw","antibot":"true", "premium_proxy":"true",  "js_render":"true",}
 
-# response = client.get(url, params=params)
+response = client.get(url, params=params) #.decode()
 
-# print(response.text)
+print(response.text)
