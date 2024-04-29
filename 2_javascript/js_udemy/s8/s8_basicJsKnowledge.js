cebfe8408cd93042f09a1494a5faba055b2de0ea
RaphaFang <fangsihyu0211@gmail.com>
2024-04-29 23:05:54 +0800

    TDZ

--- 2_javascript/js_udemy/s8/s8_basicJsKnowledge.js
+++ 2_javascript/js_udemy/s8/s8_basicJsKnowledge.js
@@ -1,8 +1,12 @@
-// beside global scope, all the scope are local
+// 90. js is a JIT language
+// just in time compilation 一邊邊陳一邊執行
 
+// beside global scope, all the scope are local
 // scope chain
 // one way only look "up"
 
+// 94. hoisting，總之不要用 "var"，11:50 他只是一個byproduct
+// TDZ, temporal dead zone, 暫時死區，JS讀變數，即便上方沒有會讀下方，如果有也會執行
 "use strict";
 
 // var variable is outside the current scope
